7f384566f0ea03026004cf52ccd19af89ce2f457
pygosuperman <pygosuperman@outlook.com>
2021-12-17 09:54:36 +0800

    支持异步

--- examples/demo02_async/master.py
+++ examples/demo02_async/master.py
@@ -1,38 +1,42 @@
 """
 从服务端获取100台机组的数据，每台机组有25个变量
 """
-from zdpapi_modbus import MasterAsync
-import time, asyncio
+from zdpapi_modbus import MasterAsync, trans_int_to_float
+import time
+import asyncio
 
 master = MasterAsync("127.0.0.1", 502)
 slave_id = 1
+
 
 class Device:
     def __init__(self, address, length) -> None:
         self.master = MasterAsync("127.0.0.1", 502)
         self.address = address
         self.length = length
-    
+
     async def execute(self):
-        self.master.execute(1, 3, self.address, self.length)
+        start = time.time()
+        asyncio.sleep(0.04)
+        data = await self.master.execute(1, 3, self.address, self.length)
+        data = trans_int_to_float(data)
+        end = time.time()
+        print("读取到的数据是：", data)
+        print("单次读取耗时: ", end - start)
+
 
 devices = [Device((i-1)*50, 50) for i in range(1, 101)]
+
 
 async def run():
     while True:
         # 每次读取50个
-        total_time = 0
+        start = time.time()
         tasks = []
         for device in devices:
-            start = time.time()
             tasks.append(device.execute())
-            asyncio.sleep(0.04)
-            end = time.time()
-            spend = end - start
-            print("单次读取耗时：", spend)
-            total_time += spend
         await asyncio.gather(*tasks)
-        print("总共读取耗时：", total_time)
+        print("总共读取耗时：", time.time() - start)
 
 # asyncio.run(run())
 
